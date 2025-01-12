58f594ab2559869b23a33b94d23bfa8c2ecbac19
robberbadeia <robber.badeia@gmail.com>
2025-01-12 03:15:22 -0800

    Setup project 0x02

---
+++ alx-project-2/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home: React.FC = () => {
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Welcome to alx-project-2!
+      </h1>
+    </div>
+  );
+};
+
+export default Home;
